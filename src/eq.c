83ce286e7d3837a7cce670581bb18ac13e25f130
Chakrit Wichian <chakrit@oozou.com>
2012-02-17 01:37:39 +0700

    More skeletal code.

--- src/eq.c
+++ src/eq.c
@@ -3,18 +3,38 @@
 
 // _____________________________________________________________________________
 //                                                                 D E F I N E S
-#define BOARD_WIDTH 4
-#define BOARD_SIZE (BOARD_WIDTH * BOARD_WIDTH)
+#define BOARD_W 4
+#define BOARD_H 4
+#define BOARD_SIZE (BOARD_W * BOARD_H)
 
-typedef int *Board;
+#define CHAR_BORDER '…'
+#define CHAR_BLANK '.'
+#define CHAR_QUEEN 'Q'
+
+typedef char Cell;
+typedef char *Flags;
+typedef char *Board;
 typedef char *String;
+
+// our coordinate system
+int xy(int x, int y) { return y * BOARD_W + x; }
 
 // _____________________________________________________________________________
 //                                                             I N T E R F A C E
 void
 show_board(Board board)
 {
+  int x, y;
+  for (y = 0; y < BOARD_H; y++)
+    for (x = 0; x < BOARD_W; x++)
+    {
+      
+    }
 }
+
+// allocs
+Board alloc_board() { return (Board)malloc(sizeof(Cell) * BOARD_SIZE); }
+Flags alloc_flags() { return (Flags)malloc(sizeof(Cell) * BOARD_W); }
 
 // _____________________________________________________________________________
 //                                                                   S O L V E R
@@ -45,10 +65,20 @@ solve(Board b)
 //                                                                       M A I N
 int main()
 {
-  printf("Board width : %d\r\n", BOARD_WIDTH);
+  // banner
+  printf("Board width : %d\r\n", BOARD_W);
   printf("Board cells : %d\r\n", BOARD_SIZE);
   printf("Algorithm   : %s\r\n", alg_name());
+
+  // inits
+  Board game = alloc_board();
+
+  // solver
   solve(0);
+
+  // cleanup
+  free(game);
+
   return 0;
 }
 
